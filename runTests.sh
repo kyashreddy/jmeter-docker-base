60e1672f6cd6729dc87ee685db21f9262a00f056
Yashwanth Reddy <yashu.dsh@gmail.com>
2019-05-07 15:26:20 +0530

    Update runTests.sh

--- runTests.sh
+++ runTests.sh
@@ -1,11 +1,5 @@
 #!/bin/bash -eux
-# NOTES:
-# Required Arguments
-# Integration App IP/DNS @$1 --> URL for the integration app without (http/https and port)
-# Threads @$2 --> Test Name(ighd/pdfTest)
-# Threads @$3 --> Number of concurrent users
-# Time Duration  @$4 --> Amount of time the test will run in seconds
-echo "Input should be of the format ./runTests.sh 10.1.1.1 cortex-load.jmx <no of users> <duration in seconds> <optional wait time in milliseconds> "
+
 export URL=${1}
 export TEST=${2}
 export THREADS=${3}
